bdecd70640dea4c5744a832abbc620f3f5cc0d95
Ubuntu <isucon@isucon6-qualifier.gotd1zi25krexpx5c2nelaik3h.lx.internal.cloudapp.net>
2016-09-23 03:49:56 +0900

    Unix domain socket

--- ruby/config/unicorn/isuda.rb
+++ ruby/config/unicorn/isuda.rb
@@ -1,3 +1,10 @@
 worker_processes 5
 preload_app true
 timeout 120
+
+# Listen to unix domain socket
+listen "/tmp/unicorn_isuda.sock"
+
+# Logging
+stdout_path "../log/unicorn.isuda.stdout.log"
+stderr_path "../log/unicorn.isuda.stderr.log"
--- ruby/config/unicorn/isutar.rb
+++ ruby/config/unicorn/isutar.rb
@@ -1,3 +1,10 @@
 worker_processes 5
 preload_app true
 timeout 120
+
+# Listen to unix domain socket
+listen "/tmp/unicorn_isutar.sock"
+
+# Logging
+stdout_path "../log/unicorn.isutar.stdout.log"
+stderr_path "../log/unicorn.isutar.stderr.log"
