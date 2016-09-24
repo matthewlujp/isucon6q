6d9236714ae9ceb735b028e539d3d5512b2c641e
Ubuntu <isucon@isucon6-qualifier.gotd1zi25krexpx5c2nelaik3h.lx.internal.cloudapp.net>
2016-09-24 14:51:38 +0900

    Introduce kataribe access analysis

--- ruby/config/unicorn/isuda.rb
+++ ruby/config/unicorn/isuda.rb
@@ -6,5 +6,5 @@ timeout 120
 listen "/tmp/unicorn_isuda.sock"
 
 # Logging
-stdout_path "../log/unicorn.isuda.stdout.log"
-stderr_path "../log/unicorn.isuda.stderr.log"
+stdout_path "../log/unicorn/isuda.stdout.log"
+stderr_path "../log/unicorn/isuda.stderr.log"
